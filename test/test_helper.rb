0d8b5ae72c1c31541f21626d3b5ceb1356dd6b7c
uchihiro <tohchi4122@gmail.com>
2026-05-26 21:50:29 +0900

    変更

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,11 @@
 ENV["RAILS_ENV"] ||= "test"
+
+require 'coveralls'
+Coveralls.wear!('rails')
+
+require 'simplecov'
+SimpleCov.start 'rails'
+
 require_relative "../config/environment"
 require "rails/test_help"
 require 'minitest/mock'
@@ -7,7 +14,7 @@ require_relative 'sign_in_helper'
 module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors)
+    # parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
 
